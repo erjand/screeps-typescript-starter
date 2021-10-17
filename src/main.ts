7e9485b56968658e7c2e03f07bbb6a567091aa92
Eric Anderson <anderson.eric.joseph@gmail.com>
2021-10-17 13:04:22 -0600

    Small refactor of main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,33 @@
 export const loop = () => {
-    console.log('Current game tick: ${Game.time}');
+    logGameTick();
+    cleanCreepMemory();
+    spawnCreep();
+    
+    const myCreep = Game.creeps['Harvester1'];
+    roleUpgrader.run(myCreep);
+};
 
+/**
+ * Write game ticks to stdout.
+ */
+function logGameTick() {
+    console.log('Current game tick: ${Game.time}');
+}
+
+/**
+ * Remove creeps from Memory which are no longer in the Game.
+ */
+function cleanCreepMemory() {
     for (const creep in Memory.creeps) {
         if (!(creep in Game.creeps)) {
             delete Memory.creeps[creep];
         }
     }
+}
 
+/**
+ * Spawn a starting creep.
+ */
+function spawnCreep() {
     Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, MOVE], 'Harvester1');
-
-    const myCreep = Game.creeps['Harvester1'];
-    roleUpgrader.run(myCreep);
-};
+}
